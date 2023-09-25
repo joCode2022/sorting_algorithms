010c22d7e582190e8eab80121009d808ba472797
joCode <105461756+joCode2022@users.noreply.github.com>
2023-09-25 16:52:59 +0100

    0x1B. C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,6 @@
-
-
 #include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * bubble_sort - Sort by swapping largest items to end of array
  * @array: Array to be sorted
@@ -11,7 +8,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t j;
+	size_t w;
 	int swapped;
 	size_t temp;
 	size_t tempsize;
@@ -23,19 +20,19 @@ void bubble_sort(int *array, size_t size)
 	while (swapped)
 	{
 		swapped = 0;
-		j = 0;
-		while (j < tempsize - 1)
+		w = 0;
+		while (w < tempsize - 1)
 		{
-			if (array[j] > array[j + 1])
+			if (array[w] > array[w + 1])
 			{
-				temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
+				temp = array[w + 1];
+				array[w + 1] = array[w];
+				array[w] = temp;
 				print_array(array, size);
 				swapped = 1;
 			}
-			j++;
+			w++;
 		}
-		tempsize = j;
+		tempsize = w;
 	}
 }
